2a8bb1cc6edbef4b4bf1d762eecfeca6db225d2d
sicp1 <1843862423@qq.com>
2024-06-05 14:43:12 +0800

    bb

--- src/chat_js/menu.js
+++ src/chat_js/menu.js
@@ -5,17 +5,16 @@ const items=[
   {
       key:'1',
       icon:<SettingOutlined />,
-      label:'新建对话'
+      label:'新建对话',
     }
 ]
 export default function MenuChat(){
+
     return <Menu
     style={{
       width: '10%',
     }}
-    defaultSelectedKeys={['1']}
     mode="inline"
     items={items}
-
               />
 }
